62dc4b9a362b27a7c15e489e251dee87e2c338df
vinutama <muhammadkhevin@gmail.com>
2018-09-13 17:13:25 +0700

    exercise-8

---
+++ exercise-8-urutkanAbjad.js
@@ -0,0 +1,13 @@
+function urutkanAbjad(str) {
+    var splitStr = str.split('')
+    var sort = splitStr.sort();
+    var join = sort.join('')
+    return join;
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
