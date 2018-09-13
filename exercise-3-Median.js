5297f8b0ae467451ce4fa414993b54fae0cef985
vinutama <muhammadkhevin@gmail.com>
2018-09-13 14:39:09 +0700

    adding exercise-3

---
+++ exercise-3-Median.js
@@ -0,0 +1,15 @@
+function cariMedian(arr) {
+    var median = Math.floor(arr.length / 2);
+    if (arr.length % 2 !== 0) {
+        return arr[median];
+    } else {
+        return (arr[median] + arr[median - 1]) / 2;
+    }
+}
+
+// TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
